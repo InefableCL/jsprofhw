8e2b524e92a5ca52eb41d4f9961422791b4e8374
InefableCL <inefablecl@gmail.com>
2021-02-11 19:59:08 +0300

    ads

--- js/main.js
+++ js/main.js
@@ -1,257 +1,79 @@
-//   getRequest on promise
-// let getRequest = (url) => {
-//   return new Promise((resolve, reject) => {
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("GET", url, true);
-//     xhr.onreadystatechange = () => {
-//       if (xhr.readyState === 4) {
-//         if (xhr.status !== 200) {
-//           reject("Error");
-//         } else {
-//           resolve(xhr.responseText);
-//         }
-//       }
-//     };
-//     xhr.send();
-//   });
-// };
+const API =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 
-const BASE_URL =
-  "https://raw.githubusercontent.com/lotostoi/JS2_FOR_STUDENTS/lesson3/responses/";
-
-const CATALOG_URL = { goods: BASE_URL + "catalogData.json" };
-
-const CART_URL = {
-  goods: BASE_URL + "getBasket.json",
-  addById: BASE_URL + "addToBasket.json",
-  delById: BASE_URL + "deleteFromBasket.json",
-};
-
-class Good {
-  constructor({ id, title, price }) {
-    this.id = id;
-    this.title = title;
-    this.price = price;
-    this.img =
-      "https://www.meme-arsenal.com/memes/fae5e7084042aa90eb6e86ae3590c9c1.jpg";
-  }
-  rander() {
-    return `<div class="product__item" data-id="${this.id}">
-              <img src="${this.img}" alt="img" width="300px">
-              <h3>${this.title}</h3>
-              <p>${this.price} р </p>
-              <button class="buy-btn" data-id-Add="${this.id}">Купить</button>
-            </div>`;
-  }
-}
-
-class Goods {
-  constructor(options) {
-    this.container = document.querySelector(options.mainSelector);
-    this.allGoods = [];
-    this.GoodsForShow = [];
-    this.url = options.URL;
-    this.good = options.good ? options.good : null;
-    this.cart = options.cart ? options.cart : null;
-    this._init();
-  }
-  _init() {
-    return makeRequestFetch(this.url.goods)
-      .then((data) => {
-        this.allGoods = this.GoodsForShow = data;
-        this._rander();
-      })
-      .then(() => this._handler())
-      .catch((e) => {
-        console.error(e);
+let app = new Vue({
+  el: "#app",
+  data: {
+    cartUrl: "/getBasket.json",
+    catalogUrl: "/catalogData.json",
+    products: [],
+    imgCatalog: "http://placehold.it/250x150",
+    imgCart: "https://placehold.it/50x100",
+    searchText: "",
+    filteredProducts: [],
+    showCart: false,
+    cartItems: [],
+  },
+  methods: {
+    getJson(url) {
+      return fetch(url)
+        .then((result) => result.json())
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    addProduct(product) {
+      this.getJson(`${API}/addToBasket.json`).then((data) => {
+        if (data.result === 1) {
+          let find = this.cartItems.find(
+            (el) => el.id_product === product.id_product
+          );
+          if (find) {
+            find.quantity++;
+          } else {
+            let prod = Object.assign({ quantity: 1 }, product);
+            this.cartItems.push(prod);
+          }
+        } else {
+          alert("Error");
+        }
       });
-  }
+    },
 
-  _rander() {
-    this.container.innerHTML = "";
-    this.allGoods.forEach((good) => {
-      let newGood = new this.good(good);
-      this.container.insertAdjacentHTML("beforeend", newGood.rander());
-    });
-  }
+    remove(item) {
+      this.getJson(`${API}/deleteFromBasket.json`).then((data) => {
+        if (data.result === 1) {
+          if (item.quantity > 1) {
+            item.quantity--;
+          } else {
+            this.cartItems.splice(this.cartItems.indexOf(item), 1);
+          }
+        }
+      });
+    },
 
-  _handler() {
-    this.container.addEventListener("click", (e) => {
-      if (e.target.dataset.idAdd) {
-        let item = this.allGoods.find((g) => +g.id === +e.target.dataset.idAdd);
-        this.cart.addItem(item);
+    searchFor() {
+      let text = this.searchText.toLowerCase().trim();
+
+      if (text === "") {
+        this.filteredProducts = this.products;
+      } else {
+        this.filteredProducts = this.products.filter((el) => {
+          return el.product_name.toLowerCase().includes(text);
+        });
+      }
+    },
+  },
+  mounted() {
+    this.getJson(`${API + this.catalogUrl}`).then((data) => {
+      for (let el of data) {
+        this.products.push(el);
       }
     });
-  }
-}
-
-class GoodInCart extends Good {
-  constructor({ id, title, price, img, quantity }) {
-    super({ id, title, price, img, quantity });
-    this.quantity = quantity;
-  }
-  rander() {
-    return `
-    <div class="shoppingCart-line"></div>
-    <div class="productLine">
-      <div class="productDetails">
-        <div class="productDetails__photo"><a href="#"><img src="${
-          this.img
-        }" alt="t-shirt2"></a>
-        </div>
-        <div class="productDetails__description">
-          <h2>${this.title}</h2>
-          <p>Color:<span>Red</span></p>
-          <p>Size:<span>Xll</span></p>
-        </div>
-      </div>
-      <div class="unitPrice">
-        <p class="productFeatures">$${this.price}</p>
-      </div>
-      <div class="quantity">
-        <p class="productFeatures">${this.quantity}</p>
-      </div>
-      <div class="subtotal">
-        <p class="productFeatures">${this.quantity * this.price}</p>
-      </div>
-      <div class="action" data-id-del="${this.id}">
-        <p class="productFeatures" data-id-del="${this.id}">Удалить</p>
-      </div>
-    </div>
-    `;
-  }
-}
-
-class Cart extends Goods {
-  constructor(options) {
-    super(options);
-    this.fieldsForAllSum = options.selectorsFieldsForAllSum
-      ? options.selectorsFieldsForAllSum
-      : null;
-    this.fieldsForAllQuantity = options.selectorsFieldsForAllQuantity
-      ? options.selectorsFieldsForAllQuantity
-      : null;
-  }
-  _handler() {
-    this.container.addEventListener("click", (e) => {
-      if (e.target.dataset.idDel) {
-        let item = this.allGoods.find((g) => +g.id === +e.target.dataset.idDel);
-        this.removeItem(item);
-      }
-    });
-  }
-
-  clacAllQuantity() {
-    return this.GoodsForShow.reduce((accum, g) => accum + g.quantity, 0);
-  }
-  clacAllSum() {
-    return this.GoodsForShow.reduce(
-      (accum, g) => accum + g.quantity * g.price,
-      0
-    );
-  }
-
-  _rander() {
-    super._rander();
-    setContentInElements.apply(this, [
-      this.fieldsForAllQuantity,
-      this.clacAllQuantity(),
-    ]);
-    setContentInElements.apply(this, [this.fieldsForAllSum, this.clacAllSum()]);
-  }
-  addItem(item) {
-    makeRequestFetch(this.url.addById)
-      .then(({ result }) => {
-        if (result) {
-          let good = this.GoodsForShow.find((g) => g.id === item.id);
-          if (good) {
-            good.quantity++;
-          } else {
-            this.GoodsForShow.push({ ...item, quantity: 1 });
-          }
-          this._rander();
-        } else {
-          throw new Error("Server's answer isn't correct...");
-        }
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }
-  removeItem(item) {
-    makeRequestFetch(this.url.delById)
-      .then(({ result }) => {
-        if (result) {
-          let good = this.GoodsForShow.find((g) => g.id === item.id);
-          if (good.quantity > 1) {
-            good.quantity--;
-          } else {
-            let idx = this.GoodsForShow.findIndex((g) => g.id === item.id);
-            this.GoodsForShow.splice(idx, 1);
-          }
-          this._rander();
-        } else {
-          throw new Error("Server's answer isn't correct...");
-        }
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }
-}
-
-let CartShop = new Cart({
-  mainSelector: ".CartList",
-  good: GoodInCart,
-  URL: CART_URL,
-  selectorsFieldsForAllSum: ".header__cart-sum",
-  selectorsFieldsForAllQuantity: ".header__cart-quantity",
+    this.filteredProducts = this.products;
+    // if (this.products < 1) {
+    //     document.getElementById("not_date").innerHTML = `<p>Нет данных</p>`;
+    //   } else {
+    //   }
+  },
 });
-
-new Goods({
-  mainSelector: ".catalog",
-  good: Good,
-  cart: CartShop,
-  URL: CATALOG_URL,
-});
-
-function makeRequestFetch(url) {
-  return fetch(url).then((data) => data.json());
-}
-
-function setContentInElements(selectors, value) {
-  if (typeof selectors === "string") {
-    let elements = [...document.querySelectorAll(selectors)];
-    elements.forEach((e) => {
-      e.innerHTML = value;
-    });
-  } else {
-  }
-}
-
-// class ProductItem {
-//   constructor(
-//     product,
-//     img = "https://www.meme-arsenal.com/memes/fae5e7084042aa90eb6e86ae3590c9c1.jpg"
-//   ) {
-//     this.title = product.title;
-//     this.price = product.price;
-//     this.id = product.id;
-//     this.img = img;
-//   }
-
-//   render() {
-//     return `<div class="product__item" data-id="${this.id}">
-//               <img src="${this.img}" alt="img" width="300px">
-//               <h3>${this.title}</h3>
-//               <p>${this.price} р </p>
-//               <button class="buy-btn">Купить</button>
-//             </div>`;
-//   }
-// }
-
-// class cart {
-//   cartlist = [];
-
-// }
-// const productList = new ProductList();
